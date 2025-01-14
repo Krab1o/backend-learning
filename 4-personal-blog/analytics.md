85e156853f3946859c7b50e4f3e82dd5997f150d
Krab1o <bogdansils@gmail.com>
2025-01-14 16:42:33 +0400

    blog continue

---
+++ 4-personal-blog/analytics.md
@@ -0,0 +1,19 @@
+# Requirements
+
+- [ ] Separate API and HTML endpoints.
+- [ ] Create methods on particular endpoints
+- [ ] Return web-pages on particular endpoints (sub)
+- [ ] Create authorization on login endpoint (/login)
+- [ ] Create admin panel
+- [ ] Store data in Postgres database
+- [ ] User Redis as Rate Limiter and probably caching
+- [ ] Dockerize application
+- [ ] Authorization with Google
+
+
+EndPoints: (I need to separate api and html)
+1. /login
+2. /home 
+3. /article (GET, POST, PUT, DELETE), 
+4. /article/{id} (GET, POST, PUT, DELETE)
+5. /admin + /admin/dashboard
--- 4-personal-blog/main.go
+++ 4-personal-blog/main.go
@@ -6,12 +6,36 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main(){
+type User struct {
+	Login 		string	`json:"login"`
+	Password 	string	`json:"password"`
+} 
+
+type LoginHandler struct {}
+
+func (l *LoginHandler) Login(c *gin.Context) {
+	
+	c.String(http.StatusOK, "Wrong Password")
+}
+
+func (l *LoginHandler) GetPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", nil)
+}
+
+func Init(r *gin.Engine) {
+	loginHandler := LoginHandler{}
+	//TODO: group methods
+	api := r.Group("/api")
+	static := r.Group("/")
+	static.Static("/static", "./static")
+	r.LoadHTMLGlob("static/templates/*")
+	
+	api.POST("/login", loginHandler.Login)
+	static.GET("/login", loginHandler.GetPage)
+}
+
+func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	Init(r)
 	r.Run()
 }
